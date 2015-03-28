810e85760c75346071b2536197062e69a6aa40f1
Sandy <amwelch@umich.edu>
2015-03-28 11:06:17 -0400

    need to set non-blocking sockets

--- webserver.py
+++ webserver.py
@@ -2,7 +2,9 @@
 
 import gevent
 import socket
-
+import fcntl
+import os
+import errno
 
 MAX_CONN = 5
 MAX_MSG_LEN = 2048
@@ -23,9 +25,18 @@ def handle_connections(s):
     '''
 
     while 1:
-        client, addr = s.accept()
-        gevent.spawn(handle_client, client, addr)
-        gevent.sleep(0)
+        try:
+            client, addr = s.accept()
+            fcntl.fcntl(client, fcntl.F_SETFL, os.O_NONBLOCK)
+            gevent.spawn(handle_client, client, addr)
+        except socket.error as e:
+            err = e.args[0]
+            if err == errno.EAGAIN or err == errno.EWOULDBLOCK:
+                gevent.sleep(0)
+                continue
+            else:
+                print "socket error ", e
+                return
         
 
 def handle_client(c, addr):
@@ -33,14 +44,22 @@ def handle_client(c, addr):
     Prints out the client's message and acks the response
     '''
     print "Client {}:{} connected".format(addr[0], addr[1])
-    msg = "\n"
-    gevent.sleep(0)
-    while msg:
-        msg = c.recv(MAX_MSG_LEN)
-        print "MSG from {}:{}:".format(addr[0], addr[1])
-        print msg
-        c.send("ACK\n")
-        gevent.sleep(0)
+    msg = "a"
+    while msg.strip():
+        try:
+            msg = c.recv(MAX_MSG_LEN)
+            print "MSG from {}:{}:".format(addr[0], addr[1])
+            print msg
+            c.send("ACK\n")
+        except socket.error as e:
+            err = e.args[0]
+            if err == errno.EAGAIN or err == errno.EWOULDBLOCK:
+                gevent.sleep(0)
+                continue
+            else:
+                print "socket error ", e
+                return
+
 
 def main():
 
@@ -49,6 +68,8 @@ def main():
 
     s = init(HOST, PORT)
     s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+    fcntl.fcntl(s, fcntl.F_SETFL, os.O_NONBLOCK)
+
     listen = gevent.spawn(handle_connections, s)
     gevent.joinall([listen])
 
