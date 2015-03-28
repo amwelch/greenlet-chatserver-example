8e825ae714c0c6f586a319d7a8ae46d4518ccf3f
Sandy <amwelch@umich.edu>
2015-03-28 10:27:20 -0400

    simple server working without greenlets

---
+++ webserver.py
@@ -0,0 +1,52 @@
+#! /usr/bin/env python
+
+import gevent
+import socket
+
+
+MAX_CONN = 5
+MAX_MSG_LEN = 2048
+
+def init(host, port, max_connections = MAX_CONN):
+    '''
+    Initializes the listening socket and returns it
+    '''
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    s.bind((host, port))
+    s.listen(max_connections)
+    return s
+
+def handle_connections(s):
+    '''
+    Loops through connections and kicks them off to greenlets 
+    to process
+    '''
+
+    while 1:
+        client, addr = s.accept()
+        handle_client(client, addr)
+        #Send to greenlet
+        
+
+def handle_client(c, addr):
+    '''
+    Prints out the client's message and acks the response
+    '''
+    print "Client {}:{} connected".format(addr[0], addr[1])
+    msg = c.recv(MAX_MSG_LEN)
+    print "MSG from {}:{}".format(addr[0], addr[1])
+    print msg
+
+    c.send("ACK")
+
+def main():
+
+    HOST = "localhost"
+    PORT = 8080
+
+    s = init(HOST, PORT)
+    handle_connections(s)
+
+if __name__ == '__main__':
+    main()
+
